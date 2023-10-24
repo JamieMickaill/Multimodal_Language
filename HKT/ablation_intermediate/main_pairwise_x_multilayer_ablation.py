1722f6fb0bd591c47bbad8f3edb1b7e4b87b2d75
jmic8397 <jmic8397@uni.sydney.edu.au>
2023-10-24 19:21:49 +1100

    fix

--- HKT/ablation_intermediate/main_pairwise_x_multilayer_ablation.py
+++ HKT/ablation_intermediate/main_pairwise_x_multilayer_ablation.py
@@ -509,7 +509,7 @@ def train_epoch(model, train_dataloader, optimizer, scheduler, loss_fct,regressi
             outputs=model(hcf)[0]
             
         elif args.model=="HKT":
-            outputs = model(input_ids, visual, acoustic,hcf, token_type_ids=segment_ids, attention_mask=input_mask,)
+            outputs = model(input_ids, visual, acoustic,hcf, attention_mask=input_mask,token_type_ids=segment_ids)
         
         
             
